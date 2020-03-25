944fec833beebde6e80f68303eaa3904382e611b
Dan Foley <dan@dan-foley.com>
2020-03-25 23:14:05 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.integer :name
       t.float :price
-      t.size :string
+      t.string :size
       t.image_url :string
     end
   end
