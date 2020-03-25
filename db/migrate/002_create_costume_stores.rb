30e1e1df3487a9baa495e807a4103d6339a6f6ef
Dan Foley <dan@dan-foley.com>
2020-03-25 23:02:13 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,14 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
 
   def change
-    
+    create_table do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :in_business
+      t.
+    end
   end
 
 end
