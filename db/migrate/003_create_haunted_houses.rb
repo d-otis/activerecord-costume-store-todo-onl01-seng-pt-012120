95e40532962c29d0f3e8e8c92b3d674905c8fb2c
Dan Foley <dan@dan-foley.com>
2020-03-25 23:07:06 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :
+    end
+  end
+
+
+end
